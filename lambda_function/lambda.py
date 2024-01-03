abdb23d3c0189f5e6c19966123264114d7721cea
Wijaya <wijaya5521@gmail.com>
2024-01-03 21:59:51 +0700

    update lambda.py

--- lambda_function/lambda.py
+++ lambda_function/lambda.py
@@ -15,3 +15,21 @@ print(f"Hasil fungsi kuadarat = {kuadrat(5)}")
 pangkat = lambda num,pow:num**pow
 print(f"Hasil pangkat = {pangkat(5,3)}")
 
+# contoh lambda function
+
+# sorting list biasa
+data_list = ["wijaya","rey","yuji"]
+data_list.sort()
+print(f"Sorted list = {data_list}")
+
+# sorting pakai panjang
+def panjang_nama(nama):
+    return len(nama)
+
+data_list.sort(key=panjang_nama)
+print(f"Sorted list by panjang = {data_list}")
+
+# sorting pakai lambda
+data_list = ["wijaya","rey","yuji"]
+data_list.sort(key=lambda nama:len(nama))
+print(f"Sorted list by panjang = {data_list}")
